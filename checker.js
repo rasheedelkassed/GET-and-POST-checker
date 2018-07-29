01cf81405580b89510f538a246133db5ac687989
RasheedElKassed <relkassed@gmail.com>
2018-07-28 17:46:36 -0700

    Added responses for POST and none requests

--- checker.js
+++ checker.js
@@ -2,10 +2,14 @@ var express = require("express");
 
 var app = express();
 var handlebars = require("express-handlebars").create({ defaultLayout: "main" });
+var bodyParser = require('body-parser');
 
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 app.set("port", 8074);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
     var getParams = [];
@@ -16,6 +20,20 @@ app.get("/", function (req, res) {
     context.dataList = getParams;
     res.render("get", context);
 
+});
+
+app.post("/" function (req, res) {
+	var postParams = [];
+    for (var p in req.query) {
+        postParams.push({ 'name': p, 'value': req.query[p] });
+    }
+	var context = {};
+    context.dataList = getParams;
+    res.render("post", context);
+});
+
+app.use(function (req, res) {
+    res.render("home", context);
 });
 
 app.use(function (req, res) {
