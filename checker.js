876cf37017abb93c130ad82bea190d61cbe0daf6
RasheedElKassed <relkassed@gmail.com>
2018-07-28 18:34:56 -0700

    Changed previous

--- checker.js
+++ checker.js
@@ -47,3 +47,7 @@ app.use(function (err, req, res, next) {
     res.status(500);
     res.render('500');
 });
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
